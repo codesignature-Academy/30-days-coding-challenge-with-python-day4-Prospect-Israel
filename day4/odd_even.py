4904a039c174e1c95edfeee4c9778eedfc813b90
Prospect Israel <prospectchiamaka38@gmail.com>
2025-11-16 18:22:32 +0100

    assignment done

---
+++ day4/odd_even.py
@@ -0,0 +1,16 @@
+numb = int(input("enter number: "))
+
+if numb % 2 == 0:
+  print("number is even")
+else:
+  print("number is odd")
+
+num = float(input("enter a number: "))
+if num > 0:
+ print("positive")
+elif num < 0:
+ print("negative")
+else:
+ print("zero")
+
+
